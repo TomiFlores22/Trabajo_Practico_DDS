80493c0ef1f860f394a80d3f879ff9a431152b00
lautifarias <413739@sistemas.frc.utn.edu.ar>
2026-06-09 19:46:38 -0300

    Primer controlador de equipos

--- backend/routes/equipos.routes.js
+++ backend/routes/equipos.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { listarEquipos } from "../controllers/equipos.controller.js";
 
 const router = Router();
+
+router.get("/", listarEquipos);
 
 export default router;
